962a719616e096cf48a501a71d9d94306ba575cf
luis <luis.herrera@siigroup.cl>
2019-08-21 11:13:26 -0400

    se crea la funcionalidad de login, con simulador

--- src/components/loginComponent/formulario.jsx
+++ src/components/loginComponent/formulario.jsx
@@ -1,43 +1,62 @@
-import React, {useState} from 'react'
+import React, {Component} from 'react'
 
 import { Form, Button } from 'react-bootstrap'
 
-export const Formulario = ({manejaClick}) => {
-    const [validar, setValidar] = useState(false)
 
-    return(
-        <Form noValidate validated={validar} >
-            <Form.Group controlId="usuario">
-                <Form.Label>Usuario</Form.Label>
-                <Form.Control
-                    required
-                    pattern="[A-Za-z0-9@\.]{5,}"
-                    type="text"
-                    placeholder="ingrese el usuario" />
-                <Form.Text className="text-muted">* eve.holt@reqres.in</Form.Text>
-                <Form.Control.Feedback type="invalid">
-                    Por favor ingrese un usuario válido
-                </Form.Control.Feedback>
-            </Form.Group>
+export class Formulario extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            validar: false
+        }
+    }
 
-            <Form.Group controlId="contrasenia">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control
-                    required
-                    type="password"
-                    placeholder="Ingrese su correo" />
-                    <Form.Text className="text-muted">* cityslicka</Form.Text>
-                <Form.Control.Feedback type="invalid">
-                    Por favor ingrese una contraseña
-                </Form.Control.Feedback>
-            </Form.Group>
+    preManejaClick(evento) {
+        const { manejaClick } = this.props
+        const { validar } = this.state
+        manejaClick(evento)
+        if(!validar){
+            this.setState({validar: true})
+        }
+    }
 
-            <Button 
-                variant="danger" 
-                type="button" 
-                onClick={() => setValidar(true)}>Ingresar_</Button>
-        </Form>
-    )
+    render(){
+        const { validar } = this.state
+        return(
+            <Form noValidate validated={validar} >
+                <Form.Group controlId="usuario">
+                    <Form.Label>Usuario</Form.Label>
+                    <Form.Control
+                        required
+                        pattern="[A-Za-z0-9@\.]{5,}"
+                        type="text"
+                        placeholder="ingrese el usuario" />
+                    <Form.Text className="text-muted">* eve.holt@reqres.in</Form.Text>
+                    <Form.Control.Feedback type="invalid">
+                        Por favor ingrese un usuario válido
+                    </Form.Control.Feedback>
+                </Form.Group>
+    
+                <Form.Group controlId="contrasenia">
+                    <Form.Label>Contraseña</Form.Label>
+                    <Form.Control
+                        required
+                        type="password"
+                        placeholder="Ingrese su correo" />
+                        <Form.Text className="text-muted">* cityslicka</Form.Text>
+                    <Form.Control.Feedback type="invalid">
+                        Por favor ingrese una contraseña
+                    </Form.Control.Feedback>
+                </Form.Group>
+    
+                <Button 
+                    variant="danger"  
+                    type="button" 
+                    onClick={ this.preManejaClick.bind(this) }>Ingresar</Button>
+            </Form>
+        )
+
+    }
 }
 
 export default Formulario
--- src/components/loginComponent/index.jsx
+++ src/components/loginComponent/index.jsx
@@ -1,7 +1,18 @@
+/**
+ * Autor: Luis Herrera
+ * Componente: LoginComponent
+ *  Estados: [
+ *      mensajes: Lista de mensajes que serán desplegados. Contiene los datos
+ *                datos que necesita el componente que lo utiliza.
+ *      tieneMensajes: Booleano que indica si los mensajes se muestran o no.
+ *  ]
+ */
+
 import React, { Component } from 'react'
 import Mensajes from './mensajes'
 import Titulo from './titulo'
 import Formulario from './formulario'
+import axios from 'axios'
 
 import { Card } from 'react-bootstrap'
 
@@ -20,8 +31,30 @@ export default class LoginComponent extends Component {
             tieneMensajes: false
         }
     }
+    manejaMensajes(estado){
+        this.setState({tieneMensajes: estado})
+    }
 
-    manejaClick(evento){
+    manejaRequest(formulario) {
+        const iptUsuario = formulario.elements[0]
+        const iptontrasenia = formulario.elements[1]
+        axios.post(
+            'https://reqres.in/api/login', 
+            {
+                "email": iptUsuario.value,
+                "password": iptontrasenia.value
+            })
+            .then(res => {
+                (res.data.token==="QpwL5tke4Pnpja7X4")?console.log("LOGEADO"):this.manejaMensajes(true)
+
+            })
+            .catch(err => {
+                this.manejaMensajes(true)
+            })
+    }
+
+    manejaClickIngresar(evento){
+        this.manejaRequest( evento.target.parentElement )
 
     }
 
@@ -30,7 +63,7 @@ export default class LoginComponent extends Component {
         return (
             <Card>
                 <Card.Header><Titulo /></Card.Header>
-                <Card.Body> <Formulario manejaClick={this.manejaClick.bind(this)} /> </Card.Body>
+                <Card.Body> <Formulario manejaClick={this.manejaClickIngresar.bind(this)} /> </Card.Body>
                 <Card.Footer>
                     { tieneMensajes? <Mensajes mensajes={mensajes} /> : <div></div> }
                 </Card.Footer>
